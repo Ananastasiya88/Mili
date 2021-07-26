9627d919bd06c69c96fd10522a313afee716717f
Anastasiya Busygina <emelyanova0488@mail.ru>
2021-07-26 20:29:05 +0700

    1

---
+++ Mili.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String[] args) {
+        int amount = 7500;
+        int rubles = 20;
+
+        int bonus = amount / rubles;
+        System.out.println(bonus);
+    }
+}
